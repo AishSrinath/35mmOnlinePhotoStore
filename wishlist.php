5df501a63055a5e6e3bb73154360dc7579b7c510
AishSrinath <39301336+AishSrinath@users.noreply.github.com>
2018-07-17 21:08:34 +0100

    test page

---
+++ wishlist.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>Wish List of <?php echo htmlentities($_GET["user"])."<br/>";?>
+   
+    // put your code here
+
+</body>
+
+</html>
