a8d96cdff9639fb1ff58574991e1453616d99da8
AishSrinath <39301336+AishSrinath@users.noreply.github.com>
2018-07-17 21:02:43 +0100

    creating index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <form name="whishlist" action="whishlist.php"></form>
+    Show wish list of:<input type="text" name="user" value=""/>
+    <input type="submit" value="Go" />
+</body>
+</html>
