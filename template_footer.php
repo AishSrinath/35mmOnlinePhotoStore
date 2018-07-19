8668742d5010144e8dbd4714ff0e1e53a4963481
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-19 21:30:13 +0100

    Building a front page with background, header and footer(to be reused)

---
+++ template_footer.php
@@ -0,0 +1,15 @@
+<div id="pageHeader"><table width=1280 border="0" cellspacing="0" cellpadding="12">
+  <tbody>
+    <tr>
+      <td width="14%">© 2018 35mm</td>
+      <td width="86%"><a href="index.php">Home</a><br>
+        <a href="intro.php">Who is 35mm for</a><br>
+        <a href="marketplace.php">Market Place</a><br>
+        <a href="myaccount.php">My Account</a></td>
+      </tr>
+    <tr>
+      <td colspan="2">&nbsp;</td><br>
+      </tr>
+  </tbody>
+	</table>
+</div>
