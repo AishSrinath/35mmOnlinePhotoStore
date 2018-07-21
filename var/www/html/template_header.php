f6ed259148d0f5976686a5048e76d07678a7743d
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-21 12:01:29 +0100

    Updated header

--- var/www/html/template_header.php
+++ var/www/html/template_header.php
@@ -2,7 +2,7 @@
   <tbody>
     <tr>
       <td width="14%"><a href="index.php"><img src="style/logo.jpg" width="75" height="60" alt="logo"/></td>
-      <td width="86%">For the artist in you</td>
+      <td width="86%">For the artist in you :)</td>
       </tr>
     <tr>
       <td bgcolor="#000000" colspan="2"><a href="index.php">Home</a> <a href="intro.php">Who is 35mm for</a> <a href="marketplace.php">Market Place</a> <a href="myaccount.php">My Account</a></td>
