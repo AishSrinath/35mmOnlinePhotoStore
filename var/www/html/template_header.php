2a88ac2b93ec9d4d02928da89058af0a94711699
Nandita <nanditakrishnamurthy7@gmail.com>
2018-07-21 14:26:51 +0100

    Updating image

--- var/www/html/template_header.php
+++ var/www/html/template_header.php
@@ -2,7 +2,7 @@
   <tbody>
     <tr>
       <td width="14%"><a href="index.php"><img src="style/logo.jpg" width="75" height="60" alt="logo"/></td>
-      <td width="86%">For the artist in you<img src="smiley.jpg" width= "20" height="20" alt="smiley"/></td>
+      <td width="86%">For the artist in you <img src="style/smiley.jpg" width= "20" height="20" alt="smiley"/></td>
       </tr>
     <tr>
         <td bgcolor="#000000" colspan="2"><a href="index.php">Home</a> <a href="intro.php">About us</a> <a href="marketplace.php">Discover</a> <a href="userregistration.php">My Account</a>
