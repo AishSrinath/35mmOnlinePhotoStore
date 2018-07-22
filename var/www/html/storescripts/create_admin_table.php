bcf9514b22cc8d9fd88b5f8aff36a82a0e35aa9b
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-22 16:29:25 +0100

    Adding the table creation scripts, aboutus placeholder and paypal integration placeholder

--- var/www/html/storescripts/create_admin_table.php
+++ var/www/html/storescripts/create_admin_table.php
@@ -5,7 +5,6 @@ $sqlcmd = "CREATE TABLE admin (
         id INT(11) NOT NULL AUTO_INCREMENT, 
         username VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
-        last_log_date date NOT NULL,
         firstname VARCHAR(255) NOT NULL,
         lastname VARCHAR(255) NOT NULL,
         email VARCHAR(50),
