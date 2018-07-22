32fa5b99a425b52dc5fecbb3d45dc12b5c9e9dd5
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-22 16:30:56 +0100

    Adding user table creation scripts, header and footer to cover the aboutus page

--- var/www/html/template_footer.php
+++ var/www/html/template_footer.php
@@ -3,8 +3,7 @@
     <tr>
       <td width="14%">© 2018 35mm</td>
       <td width="86%"><a href="index.php">Home</a><br>
-        <a href="intro.php">Who is 35mm for</a><br>
-        <a href="marketplace.php">Market Place</a><br>
+        <a href="about_us.php">About Us</a><br>
         <a href="myaccount.php">My Account</a></td>
       </tr>
     <tr>
--- var/www/html/template_header.php
+++ var/www/html/template_header.php
@@ -1,15 +1,22 @@
 <div id="pageHeader"><table width="100%" border="0" cellspacing="0" cellpadding="12">
-  <tbody>
-    <tr>
-      <td width="14%"><a href="index.php"><img src="style/logo.jpg" width="75" height="60" alt="logo"/></td>
-      <td width="86%">For the artist in you <img src="style/smiley.jpg" width= "20" height="20" alt="smiley"/></td>
-      </tr>
-    <tr>
-        <td bgcolor="#000000" colspan="2"><a href="index.php">Home</a> <a href="aboutus.php">About us</a> <a href="marketplace.php">Discover</a> <a href="userregistration.php">My Account</a>
-      <a href="buy.php">Buy</a>
-      </td>
-      </tr>
+<a href="index.php"><img src="http://35.198.90.129/style/logo.jpg" width="75" height="60" alt="logo" align="left"/><br />
+<p align="left"><i>For the artist in you</i></p><br />
+<div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="about_us.php">About Us</a>
+  <a href="discover.php">Discover</a>
+  <div class="dropdown">
+    <button class="dropbtn">My Account
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+    </div>
+  </div> 
+</div>
+  </tr>
   </tbody>
-	</table>
+  </table>
 </div>
 
