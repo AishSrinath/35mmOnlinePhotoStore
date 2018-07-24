08140cdb613f884398dfb5e0c39e7f7a46814d5e
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-24 19:35:52 +0100

    DB query test

--- var/www/html/sql_query_test.php
+++ var/www/html/sql_query_test.php
@@ -1,7 +1,7 @@
 <?php
 require 'functions.php';
 $sql = get_user('1','aditi','password');
-
+echo $sql;
 if (mysqli_num_rows($sql) > 0) {
     while($row = mysqli_fetch_assoc($sql)) {
         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
