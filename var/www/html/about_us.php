384b7e85d690f957b4baf7995ed20242b4058002
Nandita <nanditakrishnamurthy7@gmail.com>
2018-07-24 13:15:48 +0100

    Update Content

--- var/www/html/about_us.php
+++ var/www/html/about_us.php
@@ -6,14 +6,14 @@
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
 </head>
 <body>
+<div align="center" id="mainWrapper">
+	<?php include_once("template_header.php"); ?>
     <h1>About Us</h1>
 <p>Welcome to 35mm Online Photostore, your number one source for online photo shopping. We're dedicated to giving you the very best of photographs taken by professionals under various categories, with a focus on dependability, customer service and uniqueness.</p>
 
 <p>Our website is fast growing as we are passionate about what we do- photography and bet customer service.We enable people passionate about photography discover various photographs grouped under categories for their easy access. Our website provides user friendly UI for buyers and sellers so that they can enjoy shopping with ease. </p>
 
 <p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p>
-<div align="center" id="mainWrapper">
-	<?php include_once("template_header.php"); ?>
 	<?php include_once("template_footer.php"); ?>
 </div>
 </body>
