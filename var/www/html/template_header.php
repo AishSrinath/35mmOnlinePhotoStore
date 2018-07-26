6d2bb352aa61021323ab7c1be952e6cf14f99572
Nandita <nanditakrishnamurthy7@gmail.com>
2018-07-26 12:42:43 +0100

    edits

--- var/www/html/template_header.php
+++ var/www/html/template_header.php
@@ -1,6 +1,6 @@
 <div id="pageHeader"><table width="100%" border="0" cellspacing="0" cellpadding="12">
 <a href="index.php"><img src="http://35.198.90.129/style/logo.jpg" width="75" height="60" alt="logo" align="left"/><br />
-<p align="left"><i>For the artist in you</i></p><br />
+    <p align="left"><i><b>For the artist in you</b></i></p><br />
 <div class="navbar">
   <a href="index.php">Home</a>
   <a href="about_us.php">About Us</a>
