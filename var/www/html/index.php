33a510ed3aa79996d71f8de3061e0e01c4c5e239
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-28 21:06:17 +0100

    debugging session validation

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,3 +1,24 @@
+<?php 
+ob_start();
+session_start();
+//echo $_SESSION['login_id'];
+//exit;
+if (!(isset($login) && $login == true)) {
+    if (!isset($_SESSION['login_id']) || (isset($_SESSION['login_id']) && strlen($_SESSION['login_id']) <= 0)) {
+      
+        header("Location: login.php");
+    }
+    if (isset($_SESSION['EXPIRES']) && strlen($_SESSION['EXPIRES']) > 0 && $_SESSION['EXPIRES'] < time()) {
+        $_SESSION['login_username'] = "";
+        $_SESSION['login_id'] = "";
+        $_SESSION['EXPIRES'] = "";
+        
+       header("Location:login.php?Exit=exp");
+        exit;
+    }
+}
+$_SESSION['EXPIRES'] = time() + 900; // 150 seconds (2.5 mins) 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -109,11 +130,11 @@ img {
      <?php 
     if(strlen($_SESSION['login_id']) <= 0)
     {
-      
+      echo "session ID is null";
         include_once("template_header.php");
         }
         else{
-           
+           echo "session ID is NOT null................";
         echo $_SESSION['login_id'];
     include_once("postlogin_header.php");
         }
