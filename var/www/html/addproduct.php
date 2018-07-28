3301e99246aef6c362471f65f32fbd1d4bac4a0c
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-28 11:05:35 +0100

    adding debug to check the error

--- var/www/html/addproduct.php
+++ var/www/html/addproduct.php
@@ -185,11 +185,11 @@ if ($uploadOk1 == 0) {
 
 $image_large = $target_file;
 $sql= "insert into products(category,product_name,details,price,user_id,status,date_added) values('$category','$product_name','$details','$price','$user_id','1',now())";
-mysqli_query($db_connect,$sql) or die("error in insert");
+mysqli_query($db_connect,$sql) or mysqli_error($db_connect);
 $product_id = mysqli_insert_id($db_connect); 
 
 $sql1= "UPDATE  products SET image='$image_large' WHERE id='$product_id'";
-mysqli_query($db_connect,$sql1) or die("error");
+mysqli_query($db_connect,$sql1) or die("error in update");
  header('location:addproduct.php?msg=1'); 
 }
 
