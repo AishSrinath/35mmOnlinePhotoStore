c67a02cd9911f9501afaa753fce3c6e442a7fcd2
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-28 21:13:35 +0100

    adding session validation

--- var/www/html/about_us.php
+++ var/www/html/about_us.php
@@ -28,18 +28,7 @@ $_SESSION['EXPIRES'] = time() + 900; // 150 seconds (2.5 mins)
 </head>
 <body background="images/landscape/LS_1.jpg" size="cover">
 <div align="center" id="mainWrapper">
-	
-    <h1>About Us</h1>
-<p>Welcome to 35mm Online Photostore, your number one source for online photo shopping. We're dedicated to giving you the very best of photographs taken by professionals under various categories, with a focus on dependability, customer service and uniqueness.</p>
-
-<p>Our website is fast growing as we are passionate about what we do- photography and bet customer service.We enable people passionate about photography discover various photographs grouped under categories for their easy access. Our website provides user friendly UI for buyers and sellers so that they can enjoy shopping with ease. </p>
-
-<p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p>
-	<?php include_once("template_footer.php"); ?>
-</div>
-</body>
-</html>
- <?php 
+     <?php 
     if(strlen($_SESSION['login_id']) <= 0)
     {
       
@@ -51,3 +40,14 @@ $_SESSION['EXPIRES'] = time() + 900; // 150 seconds (2.5 mins)
     include_once("postlogin_header.php");
         }
     ?>
+	
+    <h1>About Us</h1>
+<p>Welcome to 35mm Online Photostore, your number one source for online photo shopping. We're dedicated to giving you the very best of photographs taken by professionals under various categories, with a focus on dependability, customer service and uniqueness.</p>
+
+<p>Our website is fast growing as we are passionate about what we do- photography and bet customer service.We enable people passionate about photography discover various photographs grouped under categories for their easy access. Our website provides user friendly UI for buyers and sellers so that they can enjoy shopping with ease. </p>
+
+<p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p>
+	<?php include_once("template_footer.php"); ?>
+</div>
+</body>
+</html>
