1e88531d356d375f359df7cca30037756cffc9c5
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-28 21:11:49 +0100

    adding session validation

--- var/www/html/about_us.php
+++ var/www/html/about_us.php
@@ -1,3 +1,24 @@
+<?php 
+ob_start();
+session_start();
+//echo $_SESSION['login_id'];
+//exit;
+if (!(isset($login) && $login == true)) {
+    if (!isset($_SESSION['login_id']) || (isset($_SESSION['login_id']) && strlen($_SESSION['login_id']) <= 0)) {
+      
+        header("Location: login.php");
+    }
+    if (isset($_SESSION['EXPIRES']) && strlen($_SESSION['EXPIRES']) > 0 && $_SESSION['EXPIRES'] < time()) {
+        $_SESSION['login_username'] = "";
+        $_SESSION['login_id'] = "";
+        $_SESSION['EXPIRES'] = "";
+        
+       header("Location:login.php?Exit=exp");
+        exit;
+    }
+}
+$_SESSION['EXPIRES'] = time() + 900; // 150 seconds (2.5 mins) 
+?>
 <!doctype html>
 <html>
 <head>
@@ -7,7 +28,7 @@
 </head>
 <body background="images/landscape/LS_1.jpg" size="cover">
 <div align="center" id="mainWrapper">
-	<?php include_once("template_header.php"); ?>
+	
     <h1>About Us</h1>
 <p>Welcome to 35mm Online Photostore, your number one source for online photo shopping. We're dedicated to giving you the very best of photographs taken by professionals under various categories, with a focus on dependability, customer service and uniqueness.</p>
 
@@ -18,3 +39,15 @@
 </div>
 </body>
 </html>
+ <?php 
+    if(strlen($_SESSION['login_id']) <= 0)
+    {
+      
+        include_once("template_header.php");
+        }
+        else{
+           
+        echo $_SESSION['login_id'];
+    include_once("postlogin_header.php");
+        }
+    ?>
