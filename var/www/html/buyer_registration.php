5e9b51dd46765de8c81a0dbafc9c46655e9764ed
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-29 20:41:26 +0100

    Fixing buyer registration for styling

--- var/www/html/buyer_registration.php
+++ var/www/html/buyer_registration.php
@@ -13,25 +13,22 @@ $fetch=  mysqli_fetch_assoc($sql_admin_query);?>
 <title>Buyer home</title>
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
 </head>
-
 <body background="images/pup.png">
-<div class="navbar">
-  <a href="index.php">Home</a>
-  <a href="about_us.php">About Us</a>
-  <a href="discover.php">Discover</a>
- 
-  <div style="float: right;">
-  <a href="#"><?php echo "Welcome  ".$fetch['firstname']; ?></a>
-  <a href="logout.php">Logout</a>
-  </div>
-      
+<div align="center" id="mainWrapper">
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="about_us.php">About Us</a>
+        <a href="discover.php">Discover</a>
+        <div style="float: right;">
+        <a href="#"><?php echo "Welcome  ".$fetch['firstname']; ?></a>
+        <a href="logout.php">Logout</a>
+        </div>       
     </div>
     <div>
-    <p style="font-size:20px;" style="text-align:center;" style="font-family:TimesNewRoman;"><i>Welcome to 35mmPhotoStore.Please select discover to browse through our exclusive photo collection.</i></p>
-	
-</div>
-<div>    
+    <p style="font-size:20px;" style="text-align:center;" style="font-family:TimesNewRoman;"><i>Welcome to 35mmPhotoStore.Please select discover to browse through our exclusive photo collection.</i></p>	
+    </div> 
 	<?php include_once("template_footer.php"); ?>
 </div>
 </body>
 </html>
+
