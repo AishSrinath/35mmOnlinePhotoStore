2b3531f7a367e6d889875bda8270fa98a7ded5de
Aiswarya G <aiswarya.g2008@gmail.com>
2018-07-29 23:44:57 +0100

    Testing notification

--- var/www/html/email_notify.php
+++ var/www/html/email_notify.php
@@ -1,5 +1,5 @@
 <?php require 'mail.php'; 
-sendmail ('Aiswarya G','aiswarya.g2008@gmail.com','TEST-NOTIFY','TEST');
+sendmail ('Nandita Krishnamurthy','10378895@mydbs.ie','TEST-NOTIFY','TEST');
 ?>
 <?php 
 ob_start();
@@ -11,6 +11,50 @@ session_start();
 <meta charset="UTF-8">
 <title>About Us</title>
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+<style>
+/* Style the tab */
+.tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+    font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+    background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+    background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+.responsive {
+    width: 100%;
+    max-width: 400px;
+    height: auto;
+}
+
+</style>
 </head>
 <body>
 <div align="center" id="mainWrapper">
@@ -35,9 +79,23 @@ session_start();
     
         
     ?>
-    <form name="shipform" id="payform" action="paypalbutton.html" method="post"> 
- <input type="submit" name="paypal" value="Proceed to payment">
+   <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="about_us.php">About Us</a>
+        <a href="discover.php">Discover</a>
+        <div style="float: right;">
+        <a href="#"><?php echo "Welcome  ".$fetch['firstname']; ?></a>
+        <a href="logout.php">Logout</a>
+        </div>  </div>
+    <div>
+        <p style="font-size:20px;font-family:Algerian;color:orange;"><i><?php echo "Thank you for choosing 35mm ".$fetch['firstname']; ?></i></p>
+        <p style="font-size:20px;font-family:Imprint MT Shadow;color:red;"><i><b>Please proceed to payment.An automated email notification has been sent to your registered email...</b></i></p>
+         </div>  
+     <form name="shipform" id="payform" action="paypalbutton.html" method="post"> 
+ <input type="submit" name="paypal" class="btn" value="Proceed to payment">
                     
-       </form> 	
+       </form> 
+</div>
+   	
 </body>
 </html>
